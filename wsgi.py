ed68ceff6b7ed387895dca96bc5cdaa131f34515
Fernando Heitor Lopes da Silva <CIANDT\fernandohl@lnb019396cps.ciandt.global>
2024-03-15 16:50:33 -0300

    Add cast in config wsgi

--- wsgi.py
+++ wsgi.py
@@ -6,8 +6,8 @@ from app.main import app
 
 application_whois = Flask('app')
 application_whois.register_blueprint(app)
-application_whois.secret_key = config('SECRET_KEY')
+application_whois.secret_key = config('SECRET_KEY', cast=str)
 csrf = CSRFProtect(application_whois)
 
 if __name__ == "__main__":
-    application_whois.run(debug=config('DEBUG'))
+    application_whois.run(debug=config('DEBUG', cast=bool))
